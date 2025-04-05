90f7d02fe6cd5d050d1c69e2df6008fe0cdd1cd2
zzzcta <zzzctadev@gmail.com>
2025-04-05 18:26:25 +0200

    Update transform_state.gd

--- _Scripts/player/transform_state.gd
+++ _Scripts/player/transform_state.gd
@@ -7,6 +7,11 @@ func enter():
 	actor.blood.visible = true
 	actor.speed *= 1.15
 	actor.can_transform = false
+	actor.collision_shape.scale = Vector2(0.2, 0.2)
+	actor.collision_shape.rotation_degrees = 90.0
+	actor.set_collision_layer_value(2, false)
+	actor.set_collision_layer_value(5, true)
+	actor.set_collision_mask_value(3, false)
 	actor.is_transformed = true
 	await get_tree().create_timer(actor.transform_duration).timeout
 	actor.is_transformed = false
@@ -24,10 +29,17 @@ func update(_delta: float):
 	
 func physics_update(delta: float) -> void:
 	
+	if actor.is_on_floor() and Input.is_action_just_pressed("jump"):
+		actor.jump()
+		
+	actor.move()
+	
 	# Aplicamos gravedad para asegurarnos de que el jugador se mantenga en el suelo
 	actor.apply_gravity(delta)
-	# Movemos el personaje (necesario para que la gravedad funcione)
-	actor.move()
+	
+	# necesario para que la gravedad funcione
+	actor.move_and_slide()
+	
 	
 func handle_input(_event: InputEvent) -> void:
 	
@@ -39,4 +51,9 @@ func exit() -> void:
 	actor.sprite.visible = true
 	actor.blood.visible = false
 	actor.speed /= 1.15
+	actor.collision_shape.scale = Vector2(1, 1)
+	actor.collision_shape.rotation_degrees = 0.0
+	actor.set_collision_layer_value(2, true)
+	actor.set_collision_layer_value(5, false)
+	actor.set_collision_mask_value(3, true)
 	
