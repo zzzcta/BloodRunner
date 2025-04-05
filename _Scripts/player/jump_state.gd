2ddcd196605bf47c6305d6d16602fd5d3200cc77
zzzcta <zzzctadev@gmail.com>
2025-04-05 18:26:18 +0200

    Update jump_state.gd

--- _Scripts/player/jump_state.gd
+++ _Scripts/player/jump_state.gd
@@ -4,8 +4,11 @@ func enter():
 	
 	super.enter()
 	
-	actor.play_animation("jump")
+	actor.play_animation("Jump")
 	
+	if actor.is_on_floor():
+		actor.jump()
+		
 	actor.on_air = true
 	
 func update(_delta: float):
@@ -14,20 +17,12 @@ func update(_delta: float):
 	
 func physics_update(delta: float) -> void:
 	
-	# No se me ocurre de otra para moverte mientras saltes sin copiar pegar el de move
 	
-	var jump_input_direction: float = Input.get_axis("move_left", "move_right")
-	
-	if jump_input_direction != 0:
-		actor.velocity.x = jump_input_direction * actor.speed
-		actor.flip_sprite(jump_input_direction < 0)
-		
-	if actor.is_on_floor():
-		actor.velocity.y -= actor.jump_force
-		
 	actor.apply_gravity(delta)
 	
 	actor.move()
+	
+	actor.move_and_slide()
 	
 	if actor.is_on_floor() and actor.velocity.x == 0:
 		transition_to("idle")
@@ -35,7 +30,6 @@ func physics_update(delta: float) -> void:
 	if actor.is_on_floor() and (abs(actor.velocity.x) > 0):
 		transition_to("move")
 
-	
 func handle_input(_event: InputEvent) -> void:
 	
 	if (Input.is_action_just_pressed("base_attack")) and (actor.can_base_attack):
