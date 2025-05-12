9117263bbf1a15a94a8a10477e459b1c91c15117
zzzcta <zzzctadev@gmail.com>
2025-05-12 21:06:44 +0200

    Update jump_state.gd

--- _Scripts/player/jump_state.gd
+++ _Scripts/player/jump_state.gd
@@ -1,44 +1,29 @@
 extends State
 
 func enter():
-	
-	super.enter()
-	
-	actor.play_animation("Jump")
+	actor.play_animation(actor.animation_player, "jump")
 	
 	if actor.is_on_floor():
 		actor.jump()
-		
-	actor.on_air = true
 	
 func update(_delta: float):
 	pass
 	
-	
 func physics_update(delta: float) -> void:
-	
-	
 	actor.apply_gravity(delta)
 	
 	actor.move()
 	
-	actor.move_and_slide()
-	
-	if actor.is_on_floor() and actor.velocity.x == 0:
-		transition_to("idle")
-	
-	if actor.is_on_floor() and (abs(actor.velocity.x) > 0):
-		transition_to("move")
-
+	if actor.velocity.y > 0:
+		transition_to("fall")
+		
 func handle_input(_event: InputEvent) -> void:
-	
-	if (Input.is_action_just_pressed("base_attack")) and (actor.can_base_attack):
+	if Input.is_action_just_pressed("base_attack") and actor.can_perform_action("base_attack"):
 		transition_to("baseattack")
-	if (Input.is_action_just_pressed("range_attack")):
+	if Input.is_action_just_pressed("range_attack") and actor.can_perform_action("range_attack"):
 		transition_to("rangeattack")
-	if (Input.is_action_just_pressed("transform")):
+	if Input.is_action_just_pressed("transform") and actor.can_perform_action("transform"):
 		transition_to("transform")
 	
 func exit() -> void:
-	
-	actor.on_air = false
+	pass
