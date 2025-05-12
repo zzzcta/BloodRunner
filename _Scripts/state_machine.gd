2f37525764b306f2f661b6659ce5e22e6770683e
zzzcta <zzzctadev@gmail.com>
2025-05-12 21:07:12 +0200

    Update state_machine.gd

--- _Scripts/state_machine.gd
+++ _Scripts/state_machine.gd
@@ -7,16 +7,16 @@ signal state_changed(state_name: String)
 var actor: CharacterBody2D = null # El objeto que posee esta maquina de estados
 
 var current_state: State = null # Estado activo actualmente
-var states: Dictionary = {} # Mapeo de nombres de estados a instancias de State
+var states: Dictionary[String, Object] = {} # Mapeo de nombres de estados a instancias de State
 
 func _ready() -> void:
-	await owner.ready # Esperamos a que el nodo padre este reeeady
+	await owner.ready # Esperamos a que el nodo padre este reeeady 
 	actor = owner as CharacterBody2D  # Guardamos una referencia al dueño de esta maquina de estados
 	# Si nuestro actor no es un tipo CharacterBody2D crash
-	if not actor: 
+	if !actor: 
 		push_error("actor is not CharacterBody2D")
 		return
-	 # Registramos los estados hijos
+	# Registramos los estados hijos 
 	for child in get_children():
 		if child is State: # Solo guardaremos los hijos que sean extiendan la clase States
 			var state: State = child as State
@@ -34,7 +34,6 @@ func _ready() -> void:
 		var initial_state: String = states.values()[0].name.to_lower()
 		change_state(initial_state)
 
-
 func _process(delta: float) -> void:
 	if current_state:
 		current_state.update(delta)
@@ -47,7 +46,7 @@ func _input(event: InputEvent) -> void:
 	if current_state:
 		current_state.handle_input(event)
 
-## Cambia el estado actual a uno nuevo
+## Gestiona los cambios entre estados
 func change_state(state_name: String) -> void:
 	# Si ya tenemos un estado activo, lo desactivamos 
 	if current_state:
@@ -59,5 +58,6 @@ func change_state(state_name: String) -> void:
 		current_state.enter()
 		# Notificamos del cambio de estado
 		emit_signal("state_changed", state_name)
+	# Notificamos error si no existe el estado
 	else:
 		push_error("Estado no encontrado: " + state_name)
