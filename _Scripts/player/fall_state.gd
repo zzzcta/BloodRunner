f48cadd9039f8a6aa2e33832e0ed9f101c3f1168
zzzcta <zzzctadev@gmail.com>
2025-05-12 21:06:40 +0200

    Create fall_state.gd

---
+++ _Scripts/player/fall_state.gd
@@ -0,0 +1,32 @@
+extends State
+
+func enter():
+	actor.play_animation(actor.animation_player, "fall")
+	
+func update(_delta: float):
+	pass
+	
+func physics_update(delta: float) -> void:
+	actor.apply_gravity(delta)
+	
+	actor.move()
+	
+	if actor.is_on_floor():
+		if actor.velocity.x == 0:
+			transition_to("idle")
+		else:
+			transition_to("move")
+		
+	if actor.velocity.y < 0:
+		transition_to("jump")
+
+func handle_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("base_attack") and actor.can_perform_action("base_attack"):
+		transition_to("baseattack")
+	if Input.is_action_just_pressed("range_attack") and actor.can_perform_action("range_attack"):
+		transition_to("rangeattack")
+	if Input.is_action_just_pressed("transform") and actor.can_perform_action("transform"):
+		transition_to("transform")
+	
+func exit() -> void:
+	pass
