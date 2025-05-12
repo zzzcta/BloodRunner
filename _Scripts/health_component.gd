9cab0f4ae00e45e5ceeba6ebced53008f1403f33
zzzcta <zzzctadev@gmail.com>
2025-05-12 21:06:20 +0200

    Update health_component.gd

--- _Scripts/health_component.gd
+++ _Scripts/health_component.gd
@@ -4,8 +4,8 @@ class_name HealthComponent
 signal died()
 
 @export var max_health: float = 10 
-var current_health: float
 
+var current_health: float
 
 func _ready() -> void:
 	current_health = max_health  
