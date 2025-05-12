fc2844351d144db7d4d5bb56c2f2d823e2159924
zzzcta <zzzctadev@gmail.com>
2025-05-12 21:07:10 +0200

    Update State.gd

--- _Scripts/State.gd
+++ _Scripts/State.gd
@@ -18,12 +18,10 @@ func physics_update(_delta: float) -> void:
 func handle_input(_event: InputEvent) -> void:
 	pass 
 	
-
 ## Se ejecuta el codigo de la funcion cuando se sale del estado
 func exit() -> void:
 	pass
 	
-
 ## Se utiliza para transicionar entre estados, se pasa como parametro el estado al cual queremos transicionar.
 func transition_to(new_state: String) -> void:
 	state_machine.change_state(new_state)
