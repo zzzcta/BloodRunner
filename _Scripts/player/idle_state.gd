747e16fff96e656837503be46b0d4647d08d8712
zzzcta <zzzctadev@gmail.com>
2025-05-12 21:06:43 +0200

    Update idle_state.gd

--- _Scripts/player/idle_state.gd
+++ _Scripts/player/idle_state.gd
@@ -1,37 +1,26 @@
-class_name IdleState
 extends State
 
 func enter():
-	
-	super.enter()
-	
-	actor.play_animation("Idle")
-	
-	actor.velocity.x = 0
+	actor.play_animation(actor.animation_player, "idle")
 	
 func update(_delta: float):
-	pass
-	
+	if !actor.is_on_floor() and actor.velocity.y > 0:
+		transition_to("fall")
+		
 func physics_update(delta: float) -> void:
 	# Aplicamos gravedad para asegurarnos de que el jugador se mantenga en el suelo
 	actor.apply_gravity(delta)
 	# Movemos el personaje (necesario para que la gravedad funcione)
 	actor.move_and_slide()
 	
-	## Bloque para que transicione de estado a salto en caso de que se este cayendo
-	#if not actor.is_on_floor() and actor.velocity.y > 0:
-		#transition_to("jump")
-	
 func handle_input(_event: InputEvent) -> void:
-	
-
-	if (Input.is_action_pressed("move_left")) or (Input.is_action_pressed("move_right")):
+	if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
 		transition_to("move")
-	if (Input.is_action_just_pressed("jump")) and (actor.is_on_floor()):
+	if Input.is_action_just_pressed("jump") and actor.is_on_floor():
 		transition_to("jump")
-	if (Input.is_action_just_pressed("base_attack")) and (actor.can_base_attack):
+	if Input.is_action_just_pressed("base_attack") and actor.can_perform_action("base_attack"):
 		transition_to("baseattack")
-	if (Input.is_action_just_pressed("range_attack")):
+	if Input.is_action_just_pressed("range_attack") and actor.can_perform_action("range_attack"):
 		transition_to("rangeattack")
-	if (Input.is_action_just_pressed("transform")):
+	if Input.is_action_just_pressed("transform") and actor.can_perform_action("transform"):
 		transition_to("transform")
