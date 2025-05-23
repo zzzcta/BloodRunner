52c17f1323fdc6bd79eab0ed9ac4e2f50b94b2f3
zzzcta <zzzctadev@gmail.com>
2025-05-23 17:43:36 +0200

    Update rejilla_ventilacion.gd

--- _Scripts/Props/rejilla_ventilacion.gd
+++ _Scripts/Props/rejilla_ventilacion.gd
@@ -5,11 +5,13 @@ var bodies_inside: Dictionary[Object, Variant] = {}
 @export var impulse_force_player_transform: float = 800
 
 func _ready():
+	AudioManager.play_sfx("fan", 150, global_position)
 	# Conectar señales para entrada y salida de cuerpos
 	$Area2D.body_entered.connect(_on_area_2d_body_entered)
 	$Area2D.body_exited.connect(_on_area_2d_body_exited)
-
+	
 func _physics_process(_delta) -> void:
+	
 	# Aplicar impulso a todos los cuerpos que esten dentro del area2D
 	for body in bodies_inside.keys():
 		if is_instance_valid(body) and body.has_node("HealthComponent"):
@@ -22,7 +24,7 @@ func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
 		var body_collision_mask: int = body.get_collision_mask()
 		
 		# Determinar la fuerza de impulso basada en la mascara de colision
-		if body_collision_mask == 9:
+		if body_collision_mask == 41:
 			impulse_force = impulse_force_player_transform
 		
 		# Registrar el cuerpo y su fuerza de impulso
