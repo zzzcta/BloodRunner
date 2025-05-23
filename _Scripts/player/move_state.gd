eff63d736001602766b2b4b2bf133f89a85b2a47
zzzcta <zzzctadev@gmail.com>
2025-05-23 17:42:44 +0200

    Update move_state.gd

--- _Scripts/player/move_state.gd
+++ _Scripts/player/move_state.gd
@@ -12,14 +12,17 @@ func physics_update(delta: float) -> void:
 	var move_direction: float = actor.move()
 	
 	# Si estamos casi detenidos, transicionamos a idle
-	if move_direction == 0 and abs(actor.velocity.x)< 5 and actor.is_on_floor():
+	if move_direction == 0 and abs(actor.velocity.x) < 5 and actor.is_on_floor():
 		transition_to("idle")
 	
 	if actor.velocity.y > 0 and !actor.is_on_floor():
 		transition_to("fall")
+		
+	if actor.velocity.y < 0:
+		transition_to("jump")
 	
 func handle_input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("jump") and actor.is_on_floor():
+	if Input.is_action_just_pressed("jump") and actor.coyote_timer_active:
 		transition_to("jump")
 	if Input.is_action_just_pressed("base_attack") and actor.can_perform_action("base_attack"):
 		transition_to("baseattack")
